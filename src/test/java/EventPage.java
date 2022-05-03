a29653d7c61fc1976e0ad34113b40c8df84417a9
Farid Naouri <fb8zan@inf.elte.hu>
2022-05-03 02:16:52 +0200

    EventPage edited to new event listener test case

--- src/test/java/EventPage.java
+++ src/test/java/EventPage.java
@@ -11,7 +11,7 @@ public class EventPage extends PageBase{
 
 	private JavascriptExecutor js=(JavascriptExecutor)driver;
     private final By categoryTravel = By.cssSelector("ul.sc-186a9jl-1.kXtDHn > li:last-child");
-    private final By selectedEvent = By.xpath("//a[@href='/hu/event/623db6ddc68c8449031432ce']");
+    private final By selectedEvent = By.xpath("//a[@href='/hu/event/624ad5e8ae297f3e9bc420f2']");
     private final By plusBtn = By.cssSelector("div.d2m8ox-0.imPzhW.fl292y-3.kKimgW > div:last-child div.o0tf15-0.hsQpR > button:last-child");
     private final By checktBtn = By.cssSelector(".sc-1g8upvm-2.bCSrMz > button");
     private final By bName = By.xpath("//input[@name='billing.name']");
@@ -20,8 +20,8 @@ public class EventPage extends PageBase{
     private final By bCity = By.xpath("//input[@name='billing.address.city']");
     private final By bCountry = By.xpath("//input[@name='billing.address.countryCode']");
     private final By nextbtn = By.xpath("//button[@class='sc-1m6grsw-0 jEVcGS']");
-    private final By attendName = By.xpath("//input[@name='NjIzZGI3N2RjNjhjODQwMzcwMTQzMzBkOjA=.name']");
-    private final By attendemail = By.xpath("//input[@name='NjIzZGI3N2RjNjhjODQwMzcwMTQzMzBkOjA=.623db7a5c68c8423d3143320']");
+    private final By attendName = By.cssSelector("input.sc-1c8q6s8-1.heGloG");
+    private final By attendemail = By.xpath("//input[@placeholder='chuck@norris.com']");
     private final By ticketTP = By.xpath("//span[@class='sc-4qvwn2-0 gPKxJU']");
     private final By paybtn = By.cssSelector("button.sc-1m6grsw-0.jEVcGS");
 	
